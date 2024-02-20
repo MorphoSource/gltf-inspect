f3f531a3dd7df78cf942f9a5c522f86bc3da3ab8
Julie Winchester <julie.winchester@duke.edu>
2024-02-20 14:59:48 -0600

    Add support for khronos extensions, add support for inspect draco gltf

--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ import pkg from '@caporal/core';
 const { program } = pkg;
 
 import { NodeIO } from '@gltf-transform/core';
+import { KHRONOS_EXTENSIONS } from '@gltf-transform/extensions';
 import { inspect } from '@gltf-transform/functions';
 
+import draco3d from 'draco3dgltf';
+
 async function parse(documentPath) {
-  let io = new NodeIO();
-  const jsonDoc = await io.readAsJSON(documentPath);
-  const document = await io.readJSON(jsonDoc);
+  const io = new NodeIO()
+    .registerExtensions(KHRONOS_EXTENSIONS)
+    .registerDependencies({
+      'draco3d.decoder': await draco3d.createDecoderModule()
+    });
+  const document = await io.read(documentPath);
   return inspect(document);
 }
 
 program
   .name("gltf-inspect")
-  .version("0.0.1")
+  .version("0.1.0")
   .description("Export JSON information about GLTF or GLB 3D assets. Based on @gltf-transform/cli's inspect command.")
   .argument("<input>", "GLTF or GLB file to inspect")
   .action(async ({ args }) => {

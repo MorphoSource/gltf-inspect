4200e3eb776543a58fcb3f2e04fba00099222faa
Julie Winchester <julie.winchester@duke.edu>
2024-02-22 15:53:17 -0600

    NodeIO mesh import now matches gltf-transform, can import more types of meshes

--- src/index.js
+++ src/index.js
@@ -4,24 +4,33 @@ import pkg from '@caporal/core';
 const { program } = pkg;
 
 import { NodeIO } from '@gltf-transform/core';
-import { KHRONOS_EXTENSIONS } from '@gltf-transform/extensions';
+import { ALL_EXTENSIONS } from '@gltf-transform/extensions';
 import { inspect } from '@gltf-transform/functions';
 
 import draco3d from 'draco3dgltf';
+import { MeshoptEncoder, MeshoptDecoder } from 'meshoptimizer';
 
 async function parse(documentPath) {
+  await MeshoptDecoder.ready;
+  await MeshoptEncoder.ready;
+  const dependencies = {
+    'draco3d.decoder': await draco3d.createDecoderModule(),
+    'draco3d.encoder': await draco3d.createEncoderModule(),
+    'meshopt.decoder': MeshoptDecoder,
+    'meshopt.encoder': MeshoptEncoder,
+  };
+
   const io = new NodeIO()
-    .registerExtensions(KHRONOS_EXTENSIONS)
-    .registerDependencies({
-      'draco3d.decoder': await draco3d.createDecoderModule()
-    });
+    .registerExtensions(ALL_EXTENSIONS)
+    .registerDependencies(dependencies);
+
   const document = await io.read(documentPath);
   return inspect(document);
 }
 
 program
   .name("gltf-inspect")
-  .version("0.1.0")
+  .version("0.2.0")
   .description("Export JSON information about GLTF or GLB 3D assets. Based on @gltf-transform/cli's inspect command.")
   .argument("<input>", "GLTF or GLB file to inspect")
   .action(async ({ args }) => {
